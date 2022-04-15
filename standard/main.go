49521d9bb1f1bc44815a33c452a1f0bee2f18653
Shingo <kawanos@google.com>
2022-04-15 20:43:52 +0900

    add the http server for standard

---
+++ standard/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	log "github.com/rs/zerolog/log"
+	// "github.com/gin-gonic/gin"
+)
+
+func init() {
+	log.Logger = zerolog.New(os.Stderr).With().Timestamp().Logger()
+	zerolog.LevelFieldName = "severity"
+	zerolog.TimestampFieldName = "timestamp"
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+}
+
+var port = os.Getenv("PORT")
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var method = r.Method
+		switch method {
+		case "GET":
+			io.WriteString(w, "Hello, World!!")
+		case "POST":
+			io.WriteString(w, "Hello, World!! for POST")
+		default:
+			io.WriteString(w, fmt.Sprintf("unsupported method: %s", r.Method))
+		}
+	})
+
+	if port == "" {
+		port = "8080"
+	}
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Error().Err(err)
+	}
+}
