93ae5a70e9dd00756f9e48dac690472b618f49f7
Shingo <kawanos@google.com>
2022-05-10 20:36:56 +0900

    just modify

--- chi/main.go
+++ chi/main.go
@@ -8,7 +8,10 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-var port = os.Getenv("PORT")
+var (
+	port = os.Getenv("PORT")
+	name = "0.01"
+)
 
 func main() {
 	r := chi.NewRouter()
@@ -16,5 +19,12 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, Koinu!"))
 	})
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	if port == "" {
+		port = "8080"
+	}
 	http.ListenAndServe(":"+port, r)
 }
