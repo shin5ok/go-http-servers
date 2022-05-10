8a397ac6e7a4b2cc90648c9c28095505f0f582d4
Shingo <kawanos@google.com>
2022-05-10 20:28:59 +0900

    start to use

---
+++ chi/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+var port = os.Getenv("PORT")
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, Koinu!"))
+	})
+	http.ListenAndServe(":"+port, r)
+}
